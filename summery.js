7aa24852179b4ad4514890be7a8da37180055e48
muntasir-i01 <you@example.communtasirm55@gmail.com>
2022-03-09 09:48:15 +0600

    local/session storage & api

---
+++ summery.js
@@ -0,0 +1,23 @@
+alert('');
+confirm('are you ok?');
+prompt('name?');
+
+/* location.reload()
+location.href = ''
+location.hash = 
+
+history.back()
+history.go()
+
+cookies
+
+sessionStorage vs localStorage
+
+cookies vs sessionStorage vs localStorage
+
+browser web api 
+clipboard api
+document.body.contentEditable = true
+
+To restart all chrome tab together
+chrome://restart*/
